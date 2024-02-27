e876ce055c1ecfd3b932daa3acda2b77c8cbb2b4
Nike Spengler <32640656+NikeSpengler@users.noreply.github.com>
2024-02-27 15:23:22 +0100

    Updating structure and styling

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -34,18 +34,18 @@ function App() {
           <DigitButton digit="1" dispatch={dispatch}/>
           <DigitButton digit="2" dispatch={dispatch}/>
           <DigitButton digit="3" dispatch={dispatch}/>
-          <OperationButton />
+          <OperationButton operation="*" dispatch={dispatch}/>
           <DigitButton digit="4" dispatch={dispatch}/>
           <DigitButton digit="5" dispatch={dispatch}/>
           <DigitButton digit="6" dispatch={dispatch}/>
-          <OperationButton/>
+          <OperationButton operation="+" dispatch={dispatch}/>
           <DigitButton digit="7" dispatch={dispatch}/>
           <DigitButton digit="8" dispatch={dispatch}/>
           <DigitButton digit="9" dispatch={dispatch}/>
-          <OperationButton/>
-          <DigitButton/>
-          <DigitButton/>
-          <button></button>
+          <OperationButton operation="-" dispatch={dispatch}/>
+          <DigitButton digit="0" dispatch={dispatch}/>
+          <DigitButton digit="." dispatch={dispatch}/>
+          <button className='span-two'onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
         </div>
       </div>
    
